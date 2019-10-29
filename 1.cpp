122427c3bb363e11a3c8c74d0cceed4e8d4777c9
Your Name <nf.pedraza@uniandes.edu.co>
2019-10-29 21:28:10 +0000

    radio

---
+++ 1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+int main(void){
+    float N;
+    std::cout<<"Ingrese el radio r del circulo"<<std::endl;
+    std::cin>>N;
+    std::cout<<"radio:"<<" "<<N<<std::endl;
+    
+    return 0;
+}
