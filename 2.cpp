52b72a86cee2cf113547b54cff9a53f077e24abc
Your Name <nf.pedraza@uniandes.edu.co>
2019-10-29 21:46:41 +0000

    lados tria

---
+++ 2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+int main(void){
+    float N1;
+    float N2;
+    float N3;
+    std::cout<<"Ingrese valor de los lados del triangulo"<<std::endl;
+    std::cin>>N1>>N2>>N3;
+    std::cout<<"Lados: "<<" "<<N1<<" "<<N2<<" "<<N3<<std::endl;
+    
+    return 0;
+}
